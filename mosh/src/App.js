959cade4e0bfaff8f16d585c37c08a33252ef1dd
Azhar Khan <107482530+Alkhanazhar@users.noreply.github.com>
2022-12-24 01:08:12 +0530

    first commit

---
+++ mosh/src/App.js
@@ -0,0 +1,13 @@
+import ToDo from './Todo';
+import ToDoItem from './TodoItem';
+
+function App() {
+  return (
+    <div>
+      <ToDo />
+      <ToDoItem />
+    </div>
+  );
+}
+
+export default App;
